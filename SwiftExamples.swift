bd333ea6e7838efd629cfca0ca1dbabcf23692d2
Ada <59454160+apzzd@users.noreply.github.com>
2022-08-12 14:20:12 -0600

    Create SwiftExamples.swift

---
+++ SwiftExamples.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+// sample functions
+
+func myFunction(_ num: Int) -> String {
+    return "Your number was \(num + 5). Just kidding! It was actually \(num)."
+}
+
+func longerFunction(_ n1: Int, _ n2: Int) -> Bool {
+    return n1 == n2
+}
+
+// sample enum
+
+enum Weather {
+    case rainy
+    case sunny
+    case foggy
+    case cloudy
+    case snowy
+}
+
+// using switch case
+
+var name = "Bob"
+
+switch name {
+case "Alice":
+    print("I know you!")
+case "Carl", "Dave", "John":
+    print("I'm sorry, what did you say your name was?")
+case "Elaine":
+    print("Hello, friend!")
+default:
+    print("Sorry, I don't know you.")
+}
+
+// using an enum (and switch case) in a function
+
+func weatherReport(_ weather: Weather) -> String {
+    switch weather {
+    case Weather.rainy:
+        return "🌧️"
+    case Weather.sunny:
+        return "☀️"
+    case Weather.foggy:
+        return "🌫️"
+    case Weather.cloudy:
+        return "☁️"
+    case Weather.snowy:
+        return "🌨️"
+    }
+}
+
